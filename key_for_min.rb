89006ce53ad9ccf8c9f48842d13145e5fd2b672b
Jacqueline Lee <anginblu@gmail.com>
2017-11-18 07:53:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,13 @@ def key_for_min_value(name_hash)
   hash_new = {key1: 0}
   name_hash.each do |k, n|
     hash_new = {key1: n}
+  end
+  name_hash.each do |k, n|
     if hash_new[:key1] < n
       hash_new = {key1: n}
     end
   end
-puts hash_new
+  puts hash_new
 end
 
 veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
