7bc6a147c2f7543f9fe7c09cebc50daf3e9a8748
Jacqueline Lee <anginblu@gmail.com>
2017-11-18 07:49:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   name_hash.each do |k, n|
     hash_new = {key1: n}
-    until hash_new[:key1] >= n
+    if hash_new[:key1] >= n
       {k: n} = hash_new
     end
   end
