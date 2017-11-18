b0a91169646667360d4d8750f05dd96db6e4afe8
Jacqueline Lee <anginblu@gmail.com>
2017-11-18 07:47:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |k, n|
-    hash = {key: n}
+    hash = {key1: n}
   end
 
   name_hash.collect do |k, n|
-    while hash[:key] < n
-      hash = {key: n}
+    while hash[:key1] < n
+      hash = {key1: n}
     end
     puts hash
   end
