5ee817aa6efe45d2d62ff571c076eef7265c795d
Jacqueline Lee <anginblu@gmail.com>
2017-11-18 07:46:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,5 +14,5 @@ def key_for_min_value(name_hash)
   end
 end
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
-key_for_min_value(ikea)
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+key_for_min_value(veggies)
